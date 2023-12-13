8b9a87b9aacabe242cd5809e4b4e04a8f98cecb2
Ziri Raha <45337531+ziriraha@users.noreply.github.com>
2023-12-13 22:38:05 +0100

    Update Group.kt
    improved debts calculation and added other functions

--- app/src/main/java/com/sdu/moneyapp/classes/Group.kt
+++ app/src/main/java/com/sdu/moneyapp/classes/Group.kt
@@ -8,23 +8,37 @@ class Group
      var name: String,
      var profilePicture: Image)
 {
-    var debts: Map<User, Map<User, Double>> = users.map({ user -> user to users.map({ user2 -> user2 to 0.0 }).toMap() }).toMap()
+    var debts: MutableMap<User, MutableMap<User, Double>> = users.associateWith { users.associateWith { 0.0 }.toMutableMap()}.toMutableMap()
+
     //var debts is the matrix of debts between users
     var expenses: List<Expense> = emptyList()
     fun addExpense(expense: Expense) {
         expenses += expense
-        calculateDebts(expense)
+        addDebtsFromExpense(expense)
     }
-    fun calculateDebts(expense: Expense) {
+    fun addDebtsFromExpense(expense: Expense) {
         //will go through the expense and calculate the debts of each user, it will then update the map of debts
+        for (payer in expense.payers) {
+            if (!debts.containsKey(payer))
+                debts[payer] = mutableMapOf()
+            if (!debts[payer]!!.contains(expense.payee))
+                debts[payer]!![expense.payee] = 0.0
+            debts[payer]!![expense.payee]?.plus(expense.amount / expense.payers.size)
+            //if the total goes from negative to positive, remove the debt
 
+        }
     }
     fun getDebts(user: User): Map<User, Double> {
-        //will return the list of debts of a user
-        return debts.get(user)!!
+        //will return the debts of a user
+        return debts[user]!!
     }
     fun addUser(user: User) {
         users += user
-        debts += user to users.map({ user2 -> user2 to 0.0 }).toMap()
+        debts[user] = mutableMapOf()
+    }
+    fun settleUp(payer: User, payee: User, amount: Double) {
+        //will settle up the debt between two users
+        debts[payer]!![payee]!!.minus(amount)
+        //if the total is negative, it means that the payer owes the payee money
     }
 }
